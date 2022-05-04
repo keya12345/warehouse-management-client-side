0e05d87c480d11f4fa7c6cb087a05b279c683055
keya12345 <0319452930@student.wub.edu.bd>
2022-05-04 22:56:28 +0600

    componet setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Home from "./componets/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>Keya</h1>
+      <Home></Home>
     </div>
   );
 }
