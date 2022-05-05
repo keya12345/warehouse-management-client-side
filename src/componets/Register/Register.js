a63dee4e34452981243c156dc8bda2a52334b5e6
keya12345 <0319452930@student.wub.edu.bd>
2022-05-05 21:17:09 +0600

    add requireauth

--- src/componets/Register/Register.js
+++ src/componets/Register/Register.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const Register = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
   const navigate = useNavigate();
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const nameRef = useRef("");
+
   const navigateLogin = () => {
     navigate("/login");
   };
+
+  if (user) {
+    navigate("/home");
+  }
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    createUserWithEmailAndPassword(email, password);
+  };
+
   return (
     <div className="container w-50 mx-auto">
       <h1 className="text-primary text-center mt-3">REGISTER</h1>
-      <Form>
+      <Form onSubmit={handleRegister}>
         <Form.Group className="mb-3" controlId="formBasicText">
           <Form.Label>Enter Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Name" />
+          <Form.Control ref={nameRef} type="text" placeholder="Enter Name" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required />
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -26,7 +52,12 @@ const Register = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" required />
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
