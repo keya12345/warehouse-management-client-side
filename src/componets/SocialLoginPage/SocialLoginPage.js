aa102227aa064e6334720fbb440e334582e2bb07
keya12345 <0319452930@student.wub.edu.bd>
2022-05-06 00:52:20 +0600

    google singin setup

--- src/componets/SocialLoginPage/SocialLoginPage.js
+++ src/componets/SocialLoginPage/SocialLoginPage.js
@@ -1,8 +1,28 @@
 import React from "react";
 import google from "../../images/google.png";
 import facebook from "../../images/facebook.png";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useNavigate } from "react-router-dom";
 
 const SocialLoginPage = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+
+  let errorMassage;
+
+  if (error) {
+    errorMassage = (
+      <div>
+        <p className="text-danger">Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  if (user) {
+    navigate("/home");
+  }
+
   return (
     <div>
       <div className="d-flex align-items-center">
@@ -10,9 +30,12 @@ const SocialLoginPage = () => {
         <p className="mt-2 px-3">or</p>
         <div style={{ height: "1px" }} className="bg-primary w-50 "></div>
       </div>
-
+      {errorMassage}
       <div>
-        <button className="btn btn-info w-50 d-block mx-auto mb-4">
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-info w-50 d-block mx-auto mb-4"
+        >
           <img height={50} width={50} src={google} alt="" />
           <span className="px-2">Google SignIn</span>
         </button>
