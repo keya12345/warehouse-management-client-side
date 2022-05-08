c8ea530b65ff54f24f7c4371a947bac574050241
keya12345 <0319452930@student.wub.edu.bd>
2022-05-08 17:45:39 +0600

    last

--- src/componets/Shared/Header/Header.js
+++ src/componets/Shared/Header/Header.js
@@ -31,22 +31,8 @@ const Header = () => {
             <Nav.Link href="home#computerCollection">
               ComputerCollection
             </Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link as={Link} to="about">
-              About
-            </Nav.Link>
             <Nav.Link as={Link} to="blog">
               Blog
             </Nav.Link>
